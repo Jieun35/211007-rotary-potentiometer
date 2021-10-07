1a54bae4951e72855d5021e73694aa8888a7baf5
Jieun35 <90733575+Jieun35@users.noreply.github.com>
2021-10-07 11:26:11 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 let 가변저항 = 0
 /**
- * 가변저항3
+ * 가변저항4
+ * 
+ * 끄기됨
  */
 function 전체끄기 () {
     pins.digitalWritePin(DigitalPin.P1, 0)
@@ -12,19 +14,15 @@ basic.forever(function () {
 })
 basic.forever(function () {
     가변저항 = pins.analogReadPin(AnalogPin.P0)
-    if (0 < 가변저항 && 가변저항 < 350) {
+    전체끄기()
+    if (50 < 가변저항 && 가변저항 < 350) {
+        전체끄기()
         pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        pins.digitalWritePin(DigitalPin.P5, 0)
     } else if (350 < 가변저항 && 가변저항 < 700) {
-        pins.digitalWritePin(DigitalPin.P1, 0)
+        전체끄기()
         pins.digitalWritePin(DigitalPin.P2, 1)
-        pins.digitalWritePin(DigitalPin.P5, 0)
     } else if (700 < 가변저항 && 가변저항 < 1023) {
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.digitalWritePin(DigitalPin.P2, 0)
+        전체끄기()
         pins.digitalWritePin(DigitalPin.P5, 1)
-    } else if (false) {
-    	
     }
 })
